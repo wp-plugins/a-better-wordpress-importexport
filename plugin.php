c7324bee26df5ebc8ef3fbb320f2cb860b16bb07
technosailor <technosailor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-05 16:22:14 +0000

    constructors and destructors
    git-svn-id: https://plugins.svn.wordpress.org/a-better-wordpress-importexport/trunk@393509 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin.php
+++ plugin.php
@@ -7,3 +7,22 @@ Author: Aaron Brazell
 Author URI: http://technosailor.com
 License: GPLv2
 */
+
+class AB_Export {
+	
+	var $is_multisite;
+	var $tables;
+	
+	function __construct() 
+	{
+		global $wpdb;
+		if( defined( 'MULTISITE') && MULTISITE )
+			$this->is_multisite = true;
+			
+		$this->tables = array( $wpdb->posts, $wpdb->postmeta, $wpdb->comments, $wpdb->commentmeta, $wpdb->links, $wpdb->options, $wpdb->term_relationships, $wpdb->term_taxonomy, $wpdb->terms, $wpdb->users, $wpdb->usermeta );
+	}
+	
+	function __destruct() {}
+	
+	
+}
