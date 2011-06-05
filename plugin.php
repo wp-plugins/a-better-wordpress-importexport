00a87667d656de6b6ce58d1448e726d99bcb8e23
technosailor <technosailor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-05 16:02:19 +0000

    Gentlemen, start your engines
    git-svn-id: https://plugins.svn.wordpress.org/a-better-wordpress-importexport/trunk@393496 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugin.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: A Better WordPress Import/Export
+Description: Provides JSON import/export for speed and fewer CPU intensive processes
+Version: 1.0-alpha
+Author: Aaron Brazell
+Author URI: http://technosailor.com
+License: GPLv2
+*/
